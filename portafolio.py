7471983dbfb8d07a75b3fe3e904be8bfae85647b
Ismael Jiménez <87533490+rcnlvr@users.noreply.github.com>
2026-09-24 17:03:40 -0600

    Update portafolio.py

--- portafolio.py
+++ portafolio.py
@@ -267,7 +267,7 @@ benchmark_options = {
 selected_benchmark = st.sidebar.selectbox("Seleccione el benchmark:", list(benchmark_options.keys()))
 benchmark = benchmark_options[selected_benchmark]
 
-st.markdown("<div style='font-size:14px; color:#888; text-align:center; margin-top:20px;'>Autores: <b>Gustavo López López</b> y <b>Ismael Omar Jiménez González</b></div>", unsafe_allow_html=True)
+st.markdown("<div style='font-size:14px; color:#888; text-align:center; margin-top:20px;'>Autor: <b>Ismael Omar Jiménez González</b></div>", unsafe_allow_html=True)
 
 
 if len(simbolos) != len(pesos) or abs(sum(pesos) - 1) > 1e-6:
